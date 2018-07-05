4d84edc1addf8a8fdf56ec55b4d09c879dc47d62
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-07-05 16:29:40 -0400

    Manually setting root

--- src/main/feature_processing/data_loader_base.py
+++ src/main/feature_processing/data_loader_base.py
@@ -21,6 +21,8 @@ class DataLoaderBase:
     @staticmethod
     def get_file_list(agg_window):
         root = os.path.dirname(sys.modules['__main__'].__file__)
+        root = "~/Projects/StudentLife-StressDetection-ML-Framework/src/main"
+
         print("Root of the Folder:", root)
         cwd = root + "/data/aggregated_data"
         student_list = os.listdir(cwd)
