c02799ff0302a3f1bc9ebb3c6a6d475cb956bf79
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-07-11 10:07:37 -0400

    Changed to bash script

--- slurm/RunRegressor.sh
+++ slurm/RunRegressor.sh
@@ -6,15 +6,15 @@
 #SBATCH --output=RegressionExp-%A.out
 #SBATCH --error=RegressionExp-%A.err
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=3
-#SBATCH --ntasks-per-node=12
-#SBATCH --time=15-00:00
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=10-00:00
 #SBATCH --mail-user=abhinavshaw@umass.edu
 
 # Thread Limiting
-export MKL_NUM_THREADS=12
-export OPENBLAS_NUM_THREADS=12
-export OMP_NUM_THREADS=12
+export MKL_NUM_THREADS=30
+export OPENBLAS_NUM_THREADS=30
+export OMP_NUM_THREADS=30
 
 # Log the jobid.
 echo $SLURM_JOBID - `hostname` >> ~/slurm-jobs.txt
