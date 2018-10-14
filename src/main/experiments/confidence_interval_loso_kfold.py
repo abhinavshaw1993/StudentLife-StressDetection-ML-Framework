b67344a9f5d045b8daf41c0da0350a9fd002e4d4
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-10-14 18:35:59 -0400

    Loop iteration for confidence interval.

--- src/main/experiments/confidence_interval_loso_kfold.py
+++ src/main/experiments/confidence_interval_loso_kfold.py
@@ -22,6 +22,7 @@ class ConfidenceIntervalsResult(ExperimentBase):
 
     def run_experiment(self, train=True, write=True, verbose=False):
         results = pd.DataFrame()
+        exp_count = 10
 
         feature_tuple = (
             ["sum", "sum", "kurtosis", "mcr", "var", "sum", "previous_stress_level", "poly_b", "poly_c", "poly_b",
@@ -45,96 +46,101 @@ class ConfidenceIntervalsResult(ExperimentBase):
         print("################################### Truncate Sq : {} ###################################".format(
             self.exp_config["truncate_sq"]))
 
-        for segragate_by_median in [True, False]:
+        for i in range(exp_count):
 
-            for splitter in self.splitter:
-                # Initiaziling Metrics lists/
+            for segragate_by_median in [True]:
 
-                accuracy = []
-                micro_f1 = []
-                macro_f1 = []
+                for splitter in self.splitter:
+                    # Initiaziling Metrics lists/
 
-                # recall
-                micro_recall = []
-                macro_recall = []
+                    accuracy = []
+                    micro_f1 = []
+                    macro_f1 = []
 
-                # Precision.
-                micro_precision = []
-                macro_precision = []
+                    # recall
+                    micro_recall = []
+                    macro_recall = []
 
-                confusion_matrix = []
+                    # Precision.
+                    micro_precision = []
+                    macro_precision = []
 
-                most_freq_accuracy = []
-                most_freq_label = []
-                print("############################## {} split running #############################".format(splitter))
+                    confusion_matrix = []
 
-                exp = GeneralizedGlobalDataLoader(agg_window=self.agg_window, splitter=splitter,
-                                                  transformer_type=self.transformer)
+                    most_freq_accuracy = []
+                    most_freq_label = []
+                    print("############################## {} split running #############################".format(splitter))
 
-                train_x, train_y, test_x, test_y, train_label_dist, test_label_dist = exp.get_data(
-                    stress_agg=self.stress_agg, previous_stress=True,
-                    feature_selection=False, verbose=verbose,
-                    segragate_by_median=segragate_by_median, truncate_sq=self.exp_config["truncate_sq"])
+                    exp = GeneralizedGlobalDataLoader(agg_window=self.agg_window, splitter=splitter,
+                                                      transformer_type=self.transformer)
 
-                # Selecting Features.
-                train_x, test_x = train_x.loc[:, feature_tuple], test_x.loc[:, feature_tuple]
+                    train_x, train_y, test_x, test_y, train_label_dist, test_label_dist = exp.get_data(
+                        stress_agg=self.stress_agg, previous_stress=True,
+                        feature_selection=False, verbose=verbose,
+                        segragate_by_median=segragate_by_median, truncate_sq=self.exp_config["truncate_sq"])
 
-                if not train:
-                    return
+                    # Selecting Features.
+                    train_x, test_x = train_x.loc[:, feature_tuple], test_x.loc[:, feature_tuple]
 
-                for classifier in self.get_ml_models():
-                    classifier_name = classifier.__class__.__name__
-                    param = self.exp_config[classifier_name]
-                    classifier.set_params(**param)
+                    if not train:
+                        return
 
-                    print("############################## {} classifier running #############################".format(
-                        classifier_name))
+                    for classifier in self.get_ml_models():
+                        classifier_name = classifier.__class__.__name__
+                        param = self.exp_config[classifier_name]
+                        classifier.set_params(**param)
 
-                    for train_idx, test_idx in exp.get_val_splitter():
-                        # Preparing splits.
-                        split_train_x, split_train_y = train_x.iloc[train_idx], train_y.iloc[train_idx]
-                        split_test_x, split_test_y = train_x.iloc[test_idx], train_y.iloc[test_idx]
+                        print("############################## {} classifier running #############################".format(
+                            classifier_name))
 
-                        # Fitting Classifier
-                        classifier.fit(split_train_x, split_train_y)
-                        split_pred_y = classifier.predict(split_test_x)
+                        for train_idx, test_idx in exp.get_val_splitter():
+                            # Preparing splits.
+                            split_train_x, split_train_y = train_x.iloc[train_idx], train_y.iloc[train_idx]
+                            split_test_x, split_test_y = train_x.iloc[test_idx], train_y.iloc[test_idx]
 
-                        ############################### Metrics ###############################
-                        accuracy.append(accuracy_score(split_test_y, split_pred_y))
+                            # Fitting Classifier
+                            classifier.fit(split_train_x, split_train_y)
+                            split_pred_y = classifier.predict(split_test_x)
 
-                        # f1 scores.
-                        micro_f1.append(f1_score(split_test_y, split_pred_y, average="micro"))
-                        macro_f1.append(f1_score(split_test_y, split_pred_y, average="macro"))
+                            ############################### Metrics ###############################
+                            accuracy.append(accuracy_score(split_test_y, split_pred_y))
 
-                        # recall
-                        micro_recall.append(recall_score(split_test_y, split_pred_y, average="micro"))
-                        macro_recall.append(recall_score(split_test_y, split_pred_y, average="macro"))
+                            # f1 scores.
+                            micro_f1.append(f1_score(split_test_y, split_pred_y, average="micro"))
+                            macro_f1.append(f1_score(split_test_y, split_pred_y, average="macro"))
 
-                        # Precision.
-                        micro_precision.append(precision_score(split_test_y, split_pred_y, average='micro'))
-                        macro_precision.append(precision_score(split_test_y, split_pred_y, average='macro'))
-                        confusion_matrix.append(cm(y_true=split_test_y, y_pred=split_pred_y))
+                            # recall
+                            micro_recall.append(recall_score(split_test_y, split_pred_y, average="micro"))
+                            macro_recall.append(recall_score(split_test_y, split_pred_y, average="macro"))
 
-                        # Generating Base line with the Given Data.
-                        mfa, mfl = ExperimentBase.generate_baseline(test_y)
-                        most_freq_accuracy.append(mfa)
-                        most_freq_label.append(mfl)
+                            # Precision.
+                            micro_precision.append(precision_score(split_test_y, split_pred_y, average='micro'))
+                            macro_precision.append(precision_score(split_test_y, split_pred_y, average='macro'))
+                            confusion_matrix.append(cm(y_true=split_test_y, y_pred=split_pred_y))
 
-                    metrics = pd.DataFrame({
-                        "segragate_by_median": segragate_by_median,
-                        "previous_stress": self.previous_stress,
-                        "splitter": splitter,
-                        "classifier": classifier_name,
-                        "avg_accuracy": sum(accuracy) / len(accuracy),
-                        "avg_micro_f1": sum(micro_f1) / len(micro_f1),
-                        "avg_macro_f1": sum(macro_f1) / len(macro_f1),
-                        "avg_micro_recall": sum(micro_recall) / len(micro_recall),
-                        "avg_macro_recall": sum(macro_recall) / len(macro_recall),
-                        "avg_micro_precision": sum(micro_precision) / len(micro_precision),
-                        "avg_macro_precision": sum(macro_precision) / len(macro_precision)},
-                        index=[0])
+                            # Generating Base line with the Given Data.
+                            mfa, mfl = ExperimentBase.generate_baseline(test_y)
+                            most_freq_accuracy.append(mfa)
+                            most_freq_label.append(mfl)
 
-                    results = results.append(metrics, ignore_index=True)
+                        metrics = pd.DataFrame({
+                            "segragate_by_median": segragate_by_median,
+                            "previous_stress": self.previous_stress,
+                            "splitter": splitter,
+                            "classifier": classifier_name,
+                            "avg_accuracy": sum(accuracy) / len(accuracy),
+                            "avg_micro_f1": sum(micro_f1) / len(micro_f1),
+                            "avg_macro_f1": sum(macro_f1) / len(macro_f1),
+                            "avg_micro_recall": sum(micro_recall) / len(micro_recall),
+                            "avg_macro_recall": sum(macro_recall) / len(macro_recall),
+                            "avg_micro_precision": sum(micro_precision) / len(micro_precision),
+                            "avg_macro_precision": sum(macro_precision) / len(macro_precision)},
+                            index=[0])
 
-        ################################ Writing to csv ################################
-        results.to_csv(path_or_buf=ROOT_DIR + "/outputs/ConfidenceInterval/results.csv")
+                        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                            print(metrics)
+
+                        results = results.append(metrics, ignore_index=True)
+
+            ################################ Writing to csv ################################
+            results.to_csv(path_or_buf=ROOT_DIR + "/outputs/ConfidenceInterval/results.csv")
