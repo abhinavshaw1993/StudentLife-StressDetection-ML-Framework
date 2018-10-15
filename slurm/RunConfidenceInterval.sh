71d58dfabf5febc1589efb6df645ae80f3dbf78f
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-10-15 00:44:30 -0400

    Bg fixes in scoring.

--- slurm/RunConfidenceInterval.sh
+++ slurm/RunConfidenceInterval.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-#SBATCH --mem=2000
+#SBATCH --mem=3000
 #SBATCH --job-name=Grid-Classif
 #SBATCH --partition=defq
 #SBATCH --output=confidence_interval-%A.out
 #SBATCH --error=confidence_interval-%A.err
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=10
-#SBATCH --ntasks-per-node=30
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=abhinavshaw@umass.edu
 
 # Thread Limiting
-export MKL_NUM_THREADS=30
-export OPENBLAS_NUM_THREADS=30
-export OMP_NUM_THREADS=30
+export MKL_NUM_THREADS=5
+export OPENBLAS_NUM_THREADS=5
+export OMP_NUM_THREADS=5
 
 # Log the jobid.
 echo $SLURM_JOBID - `hostname` >> ~/slurm-jobs.txt
